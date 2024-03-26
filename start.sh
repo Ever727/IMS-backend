e1304cd49bfb0a624bb40e0ceee35a928a8c062e
he-hj22 <he-hj22@mails.tsinghua.edu.cn>
2024-03-26 11:42:21 +0800

    build: add env settings for new app

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 manage.py makemigrations account
+python3 manage.py makemigrations account friendship
 python3 manage.py migrate
 
 # Run with uWSGI
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-coverage run --source tasright_backend,account,utils -m pytest --junit-xml=xunit-reports/xunit-result.xml
+coverage run --source tasright_backend,account,friendship,utils -m pytest --junit-xml=xunit-reports/xunit-result.xml
 ret=$?
 coverage xml -o coverage-reports/coverage.xml
 coverage report
