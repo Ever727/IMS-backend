da8f3ac7897935f4bbbe5a20cfe88cc0a130c8f1
黄宸宇 <2022010789@admin.secoder.net>
2024-04-05 01:29:23 +0800

    build: add true test

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from channels.testing import ChannelsLiveServerTestCase, WebsocketCommunicator
+from tasright_backend.asgi import application
+from asgiref.sync import sync_to_async
 
 # Create your tests here.
+
+class TrueTests(TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+
